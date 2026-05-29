821c5d215de25a9a550ff69eb3deacd0e140edf8
Nikita Tale <talenikita3@gmail.com>
2026-05-29 23:48:22 +0530

    Update project section order

--- src/App.jsx
+++ src/App.jsx
@@ -156,8 +156,8 @@ function App() {
         <Hero />
         <About />
         <Skills />
-        <Experience />
         <Projects />
+        <Experience />
         <Education />
         <Contact />
         <Footer />
